d14bb287421d03a9081f3a15f8d5065900986efd
ELREVOstudio <revsantiago31@gmail.com>
2025-03-24 21:54:00 +0800

    Tryna Connect Vercel to railway

--- src/backend/server.js
+++ src/backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // ✅ CORS Configuration
 app.use(
     cors({
-        origin: 'https://group4-3-csc-cics-conference-website-rk2l66uvc.vercel.app', // Vercel frontend URL
+        origin: 'https://group4-3-csc-cics-conference-website-iayu8jjp5.vercel.app', // Vercel frontend URL
         credentials: true, // Allow cookies to be sent across origins
     })
 );
